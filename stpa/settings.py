0a845754a148d9bd59af22f94ba504ae1b1d9654
“parn” <pjisvgreat@gmail.com>
2024-08-26 14:32:33 +0530

    sqlite3

--- stpa/settings.py
+++ stpa/settings.py
@@ -90,10 +90,11 @@ WSGI_APPLICATION = 'stpa.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',}
+}
 
 
 
@@ -108,16 +109,16 @@ WSGI_APPLICATION = 'stpa.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shanaya',
-        'USER': 'root',
-        'PASSWORD':'Pj@123456',
-        'HOST' : 'localhost',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'shanaya',
+#         'USER': 'root',
+#         'PASSWORD':'Pj@123456',
+#         'HOST' : 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
